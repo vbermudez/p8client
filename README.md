cf1fe56b85791b7dfbe2bfe3f9bf7b499bc24f95
vbermudez <vbermudez@xi-tek.com>
2015-11-18 17:05:04 +0100

    SOAP support

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # P8Client
 
+Simple and thin IBM FileNet P8 Client for Node.js
+
+##Install
 Install using:
 ```shellscript
 npm install --save https://github.com/vbermudez/p8client.git
@@ -13,3 +16,33 @@ or:
 }
 ```
 
+##Usage
+```JavaScript
+var P8Client = require('p8client')
+var P8Search = require('p8search')
+var p8 = new P8Client('http://<filenet.server.url>:<port>')
+
+p8.connect('username', 'password', function(err, client) {
+	if (err) {
+		return console.log(err)
+	}
+	
+	var search = new P8Search()
+	
+	search.objectStore = '<objectStoreID>'
+	search.query = "select * from Document where DocumentTitle like '%test%'"
+	search.searchRows = true
+	
+	client.search(search, function(err, result) {
+		if (err) {
+			return console.log(err)
+		}
+		
+		for (var i = 0, len = result.rows.length; i < len; i++) {
+			var doc = result.rows[i]
+			
+			console.log('Found', doc.DocumentTitle, 'with id', doc.Id.value)
+		}
+	})
+})
+```
--- test/spec/p8client_spec.js
+++ test/spec/p8client_spec.js
@@ -1,8 +1,8 @@
-var has_require = typeof require !== 'undefined'
+var P8Client = require('../../lib/p8client')
+var P8Search = require('../../lib/requests/p8search')
+var Document = require('../../lib/objects/p8document')
 
-if (has_require) {
-	P8Client = require('../../lib/p8client')
-}
+var OSAPPINTERNAS = '{42180535-85DC-478A-9194-792B6F12F145}'
 
 jasmine.getEnv().defaultTimeoutInterval = 20 * 1000;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
@@ -12,25 +12,79 @@ var p8 = new P8Client('http://ltesfncpe00.caf.net')
 describe('P8Client', function() {
 	it('should exists', function() {
 		expect(p8).not.toBe(null)
-		expect(p8.login).not.toBe(null)
+		expect(p8.connect).not.toBe(null)
 	})
 	
-	it('should login and get a user token', function(done) {
-		p8.login('p8admin', 'filenet', function(err, result) {
+	it('should extend correctly', function() {
+		var doc = new Document()
+		
+		expect(Document).not.toBe(null)
+		expect(doc).not.toBe(null)
+		expect(doc.properties).not.toBe(null)
+		expect(typeof doc.save === 'function').toBe(true)
+		expect(typeof doc.download === 'function').toBe(true)
+	})
+	
+	it('should connect', function(done) {
+		p8.connect('p8admin', 'filenet', function(err, client) {
 			expect(err).toBe(null)
-			expect(result).not.toBe(null)
-			expect(result.token).not.toBe(null)
+			expect(client).not.toBe(null)
+			expect(client.ws).not.toBe(null)
+			expect(client.search).not.toBe(null)
+			expect(client.ws.FNCEWS40Service).not.toBe(null)
+			expect(client.ws.FNCEWS40Service.FNCEWS40MTOMPort).not.toBe(null)
+			expect(client.ws.FNCEWS40Service.FNCEWS40MTOMPort.ExecuteSearch).not.toBe(null)
 			done()
 		})
 	})
 	
 	it('should return results', function(done) {
-		p8.search("select * from SGIIFBaseDocument where pt_caf_ejecutor = '13884'", function(err, result) {
+		var search = new P8Search()
+		
+		search.objectStore = OSAPPINTERNAS
+		search.query = "select This, * from Document where DocumentTitle like '%test%'"
+		search.searchRows = false
+		
+		p8.client.search(search, function(err, result) {
 			expect(err).toBe(null)
 			expect(result).not.toBe(null)
 			expect(result.rows).not.toBe(null)
 			expect(result.rows.length).toBeGreaterThan(0)
+			
+			// for (var i = 0, len = result.rows.length; i < len; i++) {
+			// 	var item = result.rows[i]
+				
+			// 	console.log(item.This.id, item.This.objectClass, item.This.properties)
+			// 	break
+			// }
+			
 			done()
+		})
+	})
+	
+	it('should download content', function(done) {
+		var search = new P8Search()
+		
+		search.objectStore = OSAPPINTERNAS
+		search.query = "select This, DocumentTitle from Document where DocumentTitle like '%test%'"
+		search.searchRows = false
+		
+		p8.client.search(search, function(err, result) {
+			expect(err).toBe(null)
+			expect(result).not.toBe(null)
+			expect(result.rows).not.toBe(null)
+			expect(result.rows.length).toBeGreaterThan(0)
+			
+			var doc = result.rows[0].This
+			
+			expect(doc).not.toBe(null)
+			
+			doc.download(function(err, content) {
+				expect(err).toBe(null)
+				expect(content).not.toBe(null)
+				console.log(content)
+				done()
+			})
 		})
 	})
 })
